5756032f1a3ff5aeea57a4c253853cffeb18ccd7
wuls <linsheng.wu@beantechs.com>
2022-07-01 14:28:11 +0800

    feat:esbuild vue2 plugin

---
+++ test/fixture/example.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <div id="cmd">
+    232
+  </div>
+</template>
+
+<style scoped>
+.cmd {
+  color: red;
+  list-style: none;
+  height: 100px;
+}
+</style>
--- test/middle.test.ts
+++ test/middle.test.ts
@@ -1,8 +1,16 @@
-import {  describe, expect, it } from 'vitest'
-
-describe('test middle', () => {
-  it('test parseUserAgent and test detectdevice middle', () => {
-
-    expect(1).toBe(1)
+import { describe, expect, it } from 'vitest'
+import { build } from 'esbuild'
+import plugin from '../src/index'
+describe('esbuild', () => {
+  it('build', async () => {
+    const result = await build({
+      bundle: true,
+      entryPoints: ['./fixture/example.vue'],
+      plugins: [plugin()],
+      write: false,
+    })
+    const src = String.fromCodePoint(...result.outputFiles[0].contents)
+    expect(result.outputFiles).toHaveLength(1)
+    expect(src).contain('232')
   })
 })
